8d4067f4b111706060a6527df58292d68311d5aa
umarfarooq1 <ufarooq517659@gmail.com>
2017-07-07 01:27:31 +0500

    modifications

--- config.py
+++ config.py
@@ -1,36 +1,49 @@
 import subprocess,shlex,os,sys
-
+#need to figure out this case, p2pt testlist
+#scancel 2 xfail=ticket2266 xfail=ticket2270
+passed = 0
+failed = 0
+timedout = 0
+other = 0
 mpiexec = "/home/umar/mpich-install/bin/mpiexec" + " -n "
-testlistPath = "/home/umar/Downloads/mpich-3.2/test/mpi/coll/"
-testFile = open('testlist')
+testlistPaths = ["/home/umar/Downloads/mpich-3.2/test/mpi/coll/","/home/umar/Downloads/mpich-3.2/test/mpi/pt2pt/"]
 timeLimit = 5
-testlist = testFile.read().strip().split('\n') 
-testFile.close()
 os.environ['MPIEXEC_TIMEOUT'] = str(timeLimit) #need to clarify about this
-results = open('results','a+')
 if len(sys.argv) == 1:
-	for testProg in testlist:
-		for i in range (2,5):
-			command = mpiexec + str(i)+" "+ testlistPath + testProg
-			args = shlex.split(command)
-			#print args
-			#print testProg,i
-			p = subprocess.Popen(args, stdout = subprocess.PIPE)
-			out, err = p.communicate()
-			#print out, "this"
-			if err is None and "no errors" in out.lower():
-				dat = testProg + "-n "+str(i)+"...ok\n"
-				results.write(dat)
-			elif err is not None:
-				dat = testProg + "-n "+str(i)+"...error\n Error: "+err+"\n"
-				results.write(dat)
-			elif 'timed out' in out.lower():
-				dat = testProg + "-n "+str(i)+"...timed out\n" 
-				results.write(dat)
-			else:
-				print out,err
-	 			dat = testProg + "-n "+str(i)+"...\nout:" + out + "\n"
-			results.write(dat)
+	for testlistPath in testlistPaths:
+		x = testlistPath.split('/')		
+		testFile = open(x[len(x)-2])
+		testlist = testFile.read().strip().split('\n') 
+		testFile.close()
+		results = open('results-'+x[len(x)-2],'a+')
+		for testProg in testlist:
+			for i in range (2,5):
+				command = mpiexec + str(i)+" "+ testlistPath + testProg
+				args = shlex.split(command)
+				#print args
+				#print testProg,i
+				p = subprocess.Popen(args, stdout = subprocess.PIPE)
+				out, err = p.communicate()
+				#print out, "this"
+				if err is None and "no errors" in out.lower():
+					dat = testProg + "-n "+str(i)+"...ok\n"
+					results.write(dat)
+					passed+=1
+				elif err is not None:
+					dat = testProg + "-n "+str(i)+"...error\n Error: "+err+"\n"
+					results.write(dat)
+					failed+=1
+				elif 'timed out' in out.lower():
+					dat = testProg + "-n "+str(i)+"...timed out\n" 
+					results.write(dat)
+					timedout+=1
+				else:
+					#print out,err
+		 			dat = testProg + "-n "+str(i)+"...\nout:" + out + "\n"
+					results.write(dat)
+					other+=1
+		results.close()
+		print "passed: ",passed,"failed: ",failed,"timedout: ",timedout,"other: ",other
 elif sys.argv[1] == '-p':
 	for i in range (2,5):
 		command = mpiexec + str(i)+" "+ testlistPath + sys.argv[2]
@@ -42,5 +55,3 @@ elif sys.argv[1] == '-p':
 		p = subprocess.Popen(args, stdout = subprocess.PIPE)
 		out, err = p.communicate()
 		print out,err	
-
-
